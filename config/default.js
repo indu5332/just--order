93774777678408525f3d86a4e9b2ea86eb6d330e
indu jangra <nairagarg999@gmail.com>
2021-10-19 18:04:14 +0530

    first commit

--- config/default.js
+++ config/default.js
@@ -4,10 +4,10 @@ module.exports={
     "PORT":3000,
     "DATABASE":"mongodb+srv://justorder:justorder@cluster0.ff5ml.mongodb.net/justorder?retryWrites=true&w=majority",
     "tokenDuration": "90d",
-    "DB_HOST":"https://just-server-app.herokuapp.com/",
+    "DB_HOST":"http://192.168.0.163:3000",
     "VERSION":"v1",
-    "hosts": ["*"],
-    "allowedOrigins": ["*"],
+    "hosts": ["http://192.168.0.163:3000"],
+    "allowedOrigins": ["http://192.168.0.163:3000"],
     "secret": "Pings:TakingYourExperienceToANewLevel.",
     "imagePath": path.join(__dirname,'../api/uploads/'),
     "fileUrl":"http://192.168.0.163:3000"
--- config/production.js
+++ config/production.js
@@ -1,4 +1,17 @@
-module.exports = {
+const path = require('path');
+
+module.exports={
+    "DATABASE":"mongodb+srv://justorder:justorder@cluster0.ff5ml.mongodb.net/justorder?retryWrites=true&w=majority",
+    "tokenDuration": "90d",
+    "DB_HOST":"https://just-server-app.herokuapp.com/",
+    "VERSION":"v1",
+    "hosts": ["https://just-server-app.herokuapp.com/",],
+    "allowedOrigins": ["https://just-server-app.herokuapp.com/"],
+    "secret": "Pings:TakingYourExperienceToANewLevel.",
+    "imagePath": path.join(__dirname,'../api/uploads/'),
+    "fileUrl":"https://just-server-app.herokuapp.com/"
+}
+/*module.exports = {
     NODE_ENV: process.env.NODE_ENV,
     DB_HOST: process.env.DB_HOST,
     secert: process.env.secert,
@@ -11,4 +24,4 @@ module.exports = {
     ],
     imagePath: process.env.imagePath,
   };
-  
+  */
