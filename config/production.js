6cb9d67868ffd9df77447546263380691cb8bacc
indu jangra <nairagarg999@gmail.com>
2021-10-20 15:02:04 +0530

    first commit

--- config/production.js
+++ config/production.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports={
     "DATABASE":"mongodb+srv://justorder:justorder@cluster0.ff5ml.mongodb.net/justorder?retryWrites=true&w=majority",
     "tokenDuration": "90d",
-    "DB_HOST":"https://just-server-app.herokuapp.com/",
+    "DB_HOST":"https://just-order-api.herokuapp.com/",
     "VERSION":"v1",
-    "hosts": ["https://just-server-app.herokuapp.com/",],
-    "allowedOrigins": ["https://just-server-app.herokuapp.com/"],
+    "hosts": ["https://just-order-api.herokuapp.com/",],
+    "allowedOrigins": ["https://just-order-api.herokuapp.com/"],
     "secret": "Pings:TakingYourExperienceToANewLevel.",
     "imagePath": path.join(__dirname,'../api/uploads/'),
-    "fileUrl":"https://just-server-app.herokuapp.com"
+    "fileUrl":"https://just-order-api.herokuapp.com/api/v1"
 }
