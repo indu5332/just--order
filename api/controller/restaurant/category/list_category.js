d4b62b19d914edf2b1c297570db28b000566c9dd
indu jangra <nairagarg999@gmail.com>
2021-10-21 12:53:44 +0530

    first commit

--- api/controller/restaurant/category/list_category.js
+++ api/controller/restaurant/category/list_category.js
@@ -4,6 +4,10 @@ const categorySchema=require('../../../models/category');
 let listCategory=async(req,res,next)=>{
     try {
         const allCategories = await categorySchema.find({restaurantId:mongoose.Types.ObjectId(req.decoded._id)});
+        await Promise.all(allCategories.map(async Category=>{
+            Category.image=config.fileUrl+"/"+Category.image
+            console.log(allCategories)
+        }))
         return res.status(200).json({
             success:true,
             message:'Category List',
