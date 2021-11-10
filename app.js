3f1540a70e4cfe7c208ee18b5e9c51bcf3529a47
indu jangra <indujangra@indus-MacBook-Air.local>
2021-11-10 16:53:18 +0530

    make it better

--- app.js
+++ app.js
@@ -44,4 +44,4 @@ const PORT = process.env.PORT || 3000;
 // Serve Static files
 app.use(express.static(__dirname+"/api/uploads/"));
 app.use(morgan('combined'));
-server.listen(3000,'0.0.0.0',() => console.log(`Example app listening at ${config.DB_HOST}:${PORT}/api/${config.VERSION}`));
+server.listen(process.env.PORT || 3000,'0.0.0.0',() => console.log(`Example app listening at ${config.DB_HOST}:${PORT}/api/${config.VERSION}`));
