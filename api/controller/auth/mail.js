00fea35cb4989115153aeebe2f8a6b9096d1840e
indu5332 <nairagarg999@gmail.com>
2022-02-02 16:09:04 +0530

    illfact

--- api/controller/auth/mail.js
+++ api/controller/auth/mail.js
@@ -1,21 +1,25 @@
 const sgMail = require("@sendgrid/mail");
 const config=require('config')
 
-sgMail.setApiKey(config.sgMail);
+sgMail.setApiKey(
+  config.sgMail
+);
+
 function sendEmail(data) {
   const msg = {
     to: data.receiver,
-    from: 'killingspirit5332@gmail.com',
-    subject:"reset password",
-    templateId:"d-a33144fe14294bcaa1a41e72aa00594c",
+    from: "killingspirit5332@gmail.com",
+    subject: "reset password",
+    templateId: "d-3a1855f16b96476fbce9d9f3aa76e4c0",
     dynamic_template_data: data.templateData,
-  }
+  };
   sgMail.send(msg, (error, res) => {
     if (error) {
-      console.log(error);
+      console.log(error.response.body.errors);
+      return error.response.body.errors;
     } else {
       console.log("sent", res);
     }
   });
 }
-exports.sendEmail=sendEmail;
+exports.sendEmail = sendEmail;
